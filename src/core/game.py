d2618e887e6912558df74fa89869dd39df0a2af4
RBRMN <ronnie.dov@gmail.com>
2022-08-01 23:13:06 +0300

    basic game and player

---
+++ src/core/game.py
@@ -0,0 +1,14 @@
+from player import Player
+
+class Game:
+    def __init__(self, players: list[Player]):
+        self.players = players
+
+    def announce_turn(self, player: Player):
+        print(f"It is {player.name}'s turn:")
+
+    def start(self):
+        while True:
+            for player in self.players:
+                self.announce_turn(player)
+
